4506e99727a1c824705d28412c98cfe219b06eff
yuukikiyoko <stvmgrl@aol.com>
2017-01-07 22:35:21 -0500

    Add Home Page and Navigation

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+        <!-- Include Bootstrap, Google Fonts, etc -->
+        <?php include 'header.php'; ?>
+</head>
+
+<body>
+        <!-- Include the menu bar -->
+        <?php include 'mainNavigation.php'; ?>
+</body>
+</html>
---
+++ mainNavigation.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+                <div class="navbar-header">
+                        <a class="navbar-brand" href="index.php">Travel Junkie</a>
+                </div> <!-- /navbar-header -->
+                <ul class="nav navbar-nav">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="myTravelPlans.php">My Trips</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+        </div> <!-- /container-fluid -->
+</nav>
